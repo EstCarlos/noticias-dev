d5dad5bc61451735f5040feea23bbae72ce75f09
Carloos <carlosdedios0621@gmail.com>
2023-04-18 01:10:44 -0400

    Correccion de bugs

--- components/Header.jsx
+++ components/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
           {categories.map((category) => (
             <Link key={category.slug} href={`/category/${category.slug}`}>
               <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
-                {category.name}
+                - {category.name}
               </span>
             </Link>
           ))}
